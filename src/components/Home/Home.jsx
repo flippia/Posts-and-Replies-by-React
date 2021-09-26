72539c584223d8f49eabe352b2cada34f0a4455a
flippia <79864573+flippia@users.noreply.github.com>
2021-09-26 10:28:09 -0400

    Create Home.jsx

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,42 @@
+import './Home.css';
+import '../Posts/Posts';
+import { useState, useEffect } from 'react';
+import Posts from '../Posts/Posts';
+
+const Home = () => {
+    const [ isSent, setIsSent ] = useState(true);
+    const [ posts, setPosts ] = useState(null);
+    const [ allPosts, setAllPosts ] = useState(null);
+    const [ replies, setReplies ] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/posts')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                // console.log(data);
+                setPosts(data);
+                setAllPosts(data);
+            });
+        fetch('http://localhost:8000/replies')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setReplies(data);
+            })
+    },[isSent]);
+
+    return (  
+        <div className="home">
+            {posts && posts.length > 0 ? (
+                <Posts allPosts={allPosts} posts={posts} setIsSent={setIsSent} setPosts={setPosts} replies={replies}/>
+            ):(
+                <div className="loading">Trying to access the data...</div>
+            )}
+        </div>
+    );
+}
+ 
+export default Home;
