f94e5139c98cff874111d30eb6e62a012a1499b9
flippia <79864573+flippia@users.noreply.github.com>
2021-09-26 13:03:13 -0400

    Add files via upload

---
+++ src/components/Reply/Reply.jsx
@@ -0,0 +1,55 @@
+import './Reply.css';
+import { useState } from 'react';
+
+const Reply = ({id, setIsSent}) => {
+    const [author, setAuthor] = useState('');
+    const [email, setEmail] = useState('');
+    const [body, setBody] = useState('');
+
+    const submitHandler = e => {
+        e.preventDefault();
+        const reply = { authorName: author, authorEmail: email, freeTextArea: body, postID: id };
+        if( author!=='' && email!=='' && body!=="" ){
+            fetch('http://localhost:8000/replies/', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(reply)
+            }).then(() => {
+                setAuthor('');
+                setEmail('');
+                setBody('');
+                setIsSent(isSent => isSent = !isSent);
+            });
+        }
+    }
+
+    return (  
+        <form className="make-a-reply" onSubmit={submitHandler}>
+            <div className="header">
+                <label>Replier Name:</label>
+                <input 
+                type="text" 
+                required 
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                />
+                <label>Replier Email:</label>
+                <input 
+                type="text" 
+                required 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <button onClick={submitHandler}>Reply</button>
+                </div>                                                        
+                <textarea
+                required
+                value={body}
+                placeholder="Please input here"
+                onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+        </form>
+    );
+}
+ 
+export default Reply;
