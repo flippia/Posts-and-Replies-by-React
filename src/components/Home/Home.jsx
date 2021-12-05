c9f8aea4899321573b8526a1ce36264ae1e1e5f5
flippia <79864573+flippia@users.noreply.github.com>
2021-12-05 11:26:13 -0500

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,55 +1,42 @@
-import './Reply.css';
-import { useState } from 'react';
+import './Home.css';
+import '../Posts/Posts';
+import { useState, useEffect } from 'react';
+import Posts from '../Posts/Posts';
 
-const Reply = ({id, setIsSent}) => {
-    const [author, setAuthor] = useState('');
-    const [email, setEmail] = useState('');
-    const [body, setBody] = useState('');
+const Home = () => {
+    const [ isSent, setIsSent ] = useState(true);
+    const [ posts, setPosts ] = useState(null);
+    const [ allPosts, setAllPosts ] = useState(null);
+    const [ replies, setReplies ] = useState(null);
 
-    const submitHandler = e => {
-        e.preventDefault();
-        const reply = { authorName: author, authorEmail: email, freeTextArea: body, postID: id };
-        if( author!=='' && email!=='' && body!=="" ){
-            fetch('https://intense-waters-27320.herokuapp.com/replies', {
-                method: 'POST',
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(reply)
-            }).then(() => {
-                setAuthor('');
-                setEmail('');
-                setBody('');
-                setIsSent(isSent => isSent = !isSent);
+    useEffect(() => {
+        fetch('https://intense-waters-27320.herokuapp.com/posts')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                // console.log(data);
+                setPosts(data);
+                setAllPosts(data);
             });
-        }
-    }
+        fetch('https://intense-waters-27320.herokuapp.com/replies')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setReplies(data);
+            })
+    },[isSent]);
 
     return (  
-        <form className="make-a-reply" onSubmit={submitHandler}>
-            <div className="header">
-                <label>Replier Name:</label>
-                <input 
-                type="text" 
-                required 
-                value={author}
-                onChange={(e) => setAuthor(e.target.value)}
-                />
-                <label>Replier Email:</label>
-                <input 
-                type="text" 
-                required 
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                />
-                <button onClick={submitHandler}>Reply</button>
-                </div>                                                        
-                <textarea
-                required
-                value={body}
-                placeholder="Please input here"
-                onChange={(e) => setBody(e.target.value)}
-                ></textarea>
-        </form>
+        <div className="home">
+            {posts && posts.length > 0 ? (
+                <Posts allPosts={allPosts} posts={posts} setIsSent={setIsSent} setPosts={setPosts} replies={replies} />
+            ):(
+                <div className="loading">Trying to access the data...</div>
+            )}
+        </div>
     );
 }
  
-export default Reply;
+export default Home;
