cb33f401c9802e77d66858341273ced741ee2181
flippia <79864573+flippia@users.noreply.github.com>
2021-12-05 11:16:14 -0500

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (  
         <nav className="navbar">
-            <h1>Challenge #2</h1>
+            <h1>Posts List by React</h1>
             <div className="links">
                 <NavLink exact to="/" activeClassName="active">Posts List</NavLink>
                 <NavLink exact to="/create" activeClassName="active">Create A Post</NavLink>
