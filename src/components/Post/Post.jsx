21ce23e48395890dc145e005200b666bd6f0b72b
flippia <79864573+flippia@users.noreply.github.com>
2021-12-07 19:38:53 -0500

    Update Post.jsx

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -8,7 +8,7 @@ const Post = () => {
     const history = useHistory();
 
     useEffect(() => {
-        fetch('https://intense-waters-27320.herokuapp.com/posts/' + id)
+        fetch('https://flippia-api.herokuapp.com/posts/' + id)
             .then(res => {
                 return res.json();
             })
@@ -19,7 +19,7 @@ const Post = () => {
     },[]);
 
     const handleDelete = () => {
-        fetch('https://intense-waters-27320.herokuapp.com/posts/' + post.id, {
+        fetch('https://flippia-api.herokuapp.com/posts/' + post.id, {
             method: 'DELETE'
         }).then(() => {
             history.push('/');
